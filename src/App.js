5108e2b9724617545b8d78954f70f62a1f6d64ae
prinzana <prinzana@gmail.com>
2025-02-10 23:30:59 +0100

    Google page view  Analytics

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./component/HomePage"; // Import HomePage
-import APITaskComponent from "./component/Task/APITaskComponent"
-//import BlueOceanStrategy from './component/BlueOceanStrategy'
+import APITaskComponent from "./component/Task/APITaskComponent";
+import usePageTracking from "./component/usePageTracking";
+
+const PageTracker = () => {
+  usePageTracking();
+  return null;
+};
 
 const App = () => {
   return (
     <Router>
+      {/* PageTracker must be inside the Router so that useLocation works */}
+      <PageTracker />
       <Routes>
         {/* Route to HomePage */}
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/task" element={<  APITaskComponent/>} />
-      
-
-        
-      
+        <Route path="/" element={<HomePage />} />
+        <Route path="/task" element={<APITaskComponent />} />
       </Routes>
     </Router>
   );
